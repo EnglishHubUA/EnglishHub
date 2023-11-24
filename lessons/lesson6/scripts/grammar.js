6bdd8daa30a38f665cdc5eaf754e9d81d6ac9b7f
LiliaDiak <96654700+LiliaDiak@users.noreply.github.com>
2023-11-24 16:36:11 +0200

    lessson6
    created lesson 6

---
+++ lessons/lesson6/scripts/grammar.js
@@ -0,0 +1,6 @@
+const getToKnow = ["./images/getToKnow/thereIsAre.jpg"];
+const grammar = document.querySelector('.grammar__images');
+
+        grammar.innerHTML += `
+        <img height="600px" width="900px" src="${getToKnow[0]}" alt="">
+    `
---
+++ lessons/lesson6/scripts/quiz.js
@@ -0,0 +1,153 @@
+const option1 = document.querySelector('.option1'),
+      option2 = document.querySelector('.option2'),
+      option3 = document.querySelector('.option3'),
+      option4 = document.querySelector('.option4');
+
+const optionElements = document.querySelectorAll('.option');
+
+const question = document.getElementById('question');
+
+const numberOfQuestion = document.getElementById('number-of-question'),
+      numberOfAllQuestions = document.getElementById('number-of-all-questions');
+
+let indexOfQuestion = 0, // Start with the first question
+    indexOfPage = 0;
+
+const answersTracker = document.getElementById('answers-tracker');
+const btnNext = document.getElementById('btn-next');
+
+let score = 0;
+
+const correctAnswer = document.getElementById('correct-answer'),
+      numberOfAllQuestions2 = document.getElementById('number-of-all-questions-2'),
+      btnTryAgain = document.getElementById('btn-try-again');
+
+const questions = [
+    {
+        question: "What is the name of the bear in the story?",
+        options: ["Benny", "Bobby", "Barry"],
+        rightAnswer: 0
+    },
+    {
+        "question": "What did Benny like to eat?",
+        "options": ["Fish", "Honey", "Yummy food"],
+        "rightAnswer": 1
+      },
+      {
+        "question": "How did Benny get the apples from the tree?",
+        "options": ["He asked a friend for help", "He climbed the tree", "He found them on the ground"],
+        "rightAnswer": 1
+      },
+      {
+        "question": "What did Benny find by the river?",
+        "options": ["Carrots", "Fish", "Honey"],
+        "rightAnswer": 1
+      },
+      {
+        "question": "Who did Benny share his fish with?",
+        "options": ["A friendly rabbit", "A squirrel", "No one"],
+        "rightAnswer": 0
+      },
+      {
+        "question": "What did Benny realize at the end of his food adventure?",
+        "options": ["There was a lot of delicious food in the forest", "He should eat everything by himself", "He didn't like sharing"],
+        "rightAnswer": 0
+      },
+      {
+        "question": "How did Benny and his forest friends live after his food adventure?",
+        "options": ["Happily ever after", "Hungry and sad", "In different forests"],
+        "rightAnswer": 0
+      }
+
+
+];
+
+numberOfAllQuestions.innerHTML = questions.length;
+
+const load = () => {
+    question.innerHTML = questions[indexOfQuestion].question;
+
+    option1.innerHTML = questions[indexOfQuestion].options[0];
+    option2.innerHTML = questions[indexOfQuestion].options[1];
+    option3.innerHTML = questions[indexOfQuestion].options[2];
+
+    numberOfQuestion.innerHTML = indexOfPage + 1;
+    indexOfPage++;
+};
+
+const checkAnswer = el => {
+    if (el.target.dataset.id == questions[indexOfQuestion].rightAnswer) {
+        el.target.classList.add('correct');
+        updateAnswerTracker('correct');
+        score++;
+    } else {
+        el.target.classList.add('wrong');
+        updateAnswerTracker('wrong');
+    }
+    disabledOptions();
+}
+
+for (option of optionElements) {
+    option.addEventListener('click', e => checkAnswer(e));
+}
+
+const disabledOptions = () => {
+    optionElements.forEach(item => {
+        item.classList.add('disabled');
+        if (item.dataset.id == questions[indexOfQuestion].rightAnswer) {
+            item.classList.add('correct');
+        }
+    });
+}
+
+const enableOptions = () => {
+    optionElements.forEach(item => {
+        item.classList.remove('correct', 'wrong', 'disabled');
+    });
+}
+
+const answerTracker = () => {
+    questions.forEach(() => {
+        const div = document.createElement('div');
+        answersTracker.appendChild(div);
+    });
+}
+
+const updateAnswerTracker = status => {
+    answersTracker.children[indexOfPage - 1].classList.add(`${status}`);
+}
+
+const validate = () => {
+    if (!optionElements[0].classList.contains('disabled')) {
+        alert("You need to choose one of the answer options")
+    } else {
+        indexOfQuestion++; // Move to the next question
+        if (indexOfQuestion < questions.length) {
+            load();
+            enableOptions();
+        } else {
+            quizOver();
+        }
+    }
+}
+
+const quizOver = () => {
+    document.querySelector('.quiz-over-modal').classList.add('active');
+    correctAnswer.innerHTML = score;
+    numberOfAllQuestions2.innerHTML = questions.length;
+}
+
+const tryAgain = () => {
+    window.location.reload();
+}
+
+btnTryAgain.addEventListener('click', tryAgain);
+
+btnNext.addEventListener('click', () => {
+    validate();
+});
+
+window.addEventListener('load', () => {
+    load();
+    answerTracker();
+});
